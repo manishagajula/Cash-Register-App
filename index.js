262645eeb14947b6e1b9333fbf4e41758621e470
Manisha Gajula <112008195+manishagajula@users.noreply.github.com>
2022-11-27 19:56:43 +0530

    Fix: Amount error

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage();
-  if (billAmount.value > 0) {
-    if (cashGiven.value >= billAmount.value) {
-      const amountToBeReturned = cashGiven.value - billAmount.value;
+  if (Number(billAmount.value) > 0) {
+    if (Number(cashGiven.value) >= Number(billAmount.value)) {
+      const amountToBeReturned =
+        Number(cashGiven.value) - Number(billAmount.value);
       calculateChange(amountToBeReturned);
     } else {
       showMessage("Do you wanna wash plates?");
@@ -23,7 +24,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
 function calculateChange(amountToBeReturned) {
   for (let i = 0; i < availableNotes.length; i++) {
     const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-    amountToBeReturned %= availableNotes[i];
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
     noOfNotes[i].innerText = numberOfNotes;
   }
 }
